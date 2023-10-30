a9c535db476bfe4c26cca38aecb8a57c0b56960a
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2023-10-30 11:20:07 +0200

    return static_html as card

--- metaflow_extensions/dbt_ext/plugins/cards/dbt_docs/__init__.py
+++ metaflow_extensions/dbt_ext/plugins/cards/dbt_docs/__init__.py
@@ -5,9 +5,7 @@ class DBTDocs(MetaflowCard):
     type = "dbt_docs"
 
     def render(self, task):  # this function returns the HTML to be rendered
-        return (
-            task.data.run_results
-        )  # assumes you are saving an attribute named `html` in the task
+        return task.data.static_html
 
 
 CARDS = [DBTDocs]
